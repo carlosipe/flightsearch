53f0e4310bf4e5337c5bd07891b6669973a6b46a
CarlosIPe <carlos2@compendium.com.ar>
2016-07-16 03:57:51 +0200

    Adds frontend app in Cuba

--- app.rb
+++ app.rb
@@ -2,9 +2,35 @@ require 'pg'
 require 'sequel'
 require "requests"
 require 'json'
+require "cuba"
+require "cuba/safe"
+require "mote"
+require "mote/render"
 require_relative './settings'
 DB = Sequel.connect(Settings::POSTGRESQL_URL)
 
 Dir["./models/**/*.rb"].each  { |rb| require rb }
+
+Cuba.use Rack::Static, :urls => ["/css", "/js"], :root => "public"
+Cuba.plugin(Mote::Render)
+Cuba.use Rack::Session::Cookie, secret: Settings::RACK_SECRET
+Cuba.plugin Cuba::Safe
+Dir["./routes/**/*.rb"].each  { |rb| require rb }
+
+Cuba.define do
+  on root do
+    render 'index',
+    quotes: Quote.eager(:outbound_leg_origin, :outbound_leg_destination).order_by(:min_price).all
+  end
+end
+
+# countries = %w(ES FR AR GR IT)
+# countries.each do |origin|
+#   countries.each do |destination|
+#     ['2016-07','2016-08'].each do |date|
+#       API.new.request_and_save(origin: origin, destination: destination, outbound_date: date) #optional: inbound_date
+#     end
+#   end
+# end
 
 # API.new.request_and_save(origin: 'ES', destination: 'BR', outbound_date: '2016-08') #optional: inbound_date
--- settings.rb
+++ settings.rb
@@ -4,4 +4,5 @@ module Settings
   end
   POSTGRESQL_URL      = ENV.fetch("POSTGRESQL_URL")
   SKYSCANNER_API_KEY  = ENV.fetch("SKYSCANNER_API_KEY")
+  RACK_SECRET         = ENV.fetch("RACK_SECRET")
 end
